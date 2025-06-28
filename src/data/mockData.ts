b2d4b026852f0851b15c721e40672655dc1ea069
AIExperto <maestronavarrogavilan@gmail.com>
2025-06-28 14:05:45 +0200

    Create Missing Mock Data File

---
+++ src/data/mockData.ts
@@ -0,0 +1,15 @@
+import { User, Client, ProgressEntry, Exercise, Workout, Message, Appointment } from '../types';
+
+export const mockUsers: User[] = [];
+
+export const mockClients: Client[] = [];
+
+export const mockProgressEntries: ProgressEntry[] = [];
+
+export const mockExercises: Exercise[] = [];
+
+export const mockWorkouts: Workout[] = [];
+
+export const mockMessages: Message[] = [];
+
+export const mockAppointments: Appointment[] = [];
